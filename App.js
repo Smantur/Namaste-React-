b64d9f7777740eccc99981f76f206470f5e090f9
Smantur <mssantosh0416@gmail.com>
2024-04-14 08:24:57 +0530

    completing foundation

--- App.js
+++ App.js
@@ -2,14 +2,26 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 
-const element = React.createElement("div",
-    { id: "parent" }, [React.createElement("div",
-        { id: "child" }, [React.createElement("h1",
-            {}, "I am H1"), React.createElement("h2", {}, "I am h2 tag")]), React.createElement("div",
-                { id: "child2" }, [React.createElement("h1",
-                    {}, "I am H1"), React.createElement("h2", {}, "I am h2 tag")])])
+//const element =React.createElement("h1",{id:"heading"},"Namaste React")
+// const element = (<h1 className="head"
+//     id="heading">Namaste React</h1>)
+//jsx--> javascript syntax --> convert to React element by Babel(comes along with parcel) package  which is then given to render method to create actual HTML 
+//const root = ReactDOM.createRoot(document.getElementById("root"));
 
+//JSX FUNCTIONAL COMPONENT
+/**
+ * Any functions those are returning Jsx/react Element are referred as functional component
+ */
+const Title = () => (//if JSX is multiple line use "()""
+    <h1>Hi i am Title</h1>
+);
+const Heading = () => (//<Title/> or {Title}
+    <div class="container">
+        <Title />
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element)
+        <h2>Hi i am h2 heading</h2>
+    </div>
+)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Heading />)//rendering component
+//root.render(element)//rendering React element
